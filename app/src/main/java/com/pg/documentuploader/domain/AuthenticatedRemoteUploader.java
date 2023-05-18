d7f99a0bf7f066112a3e4a6b749b6e5df97a238f
admin <admin@admins-MacBook-Pro.local>
2023-05-18 11:14:44 +0530

    documentation

--- app/src/main/java/com/pg/documentuploader/domain/AuthenticatedRemoteUploader.java
+++ app/src/main/java/com/pg/documentuploader/domain/AuthenticatedRemoteUploader.java
@@ -5,6 +5,9 @@ import android.content.Intent;
 
 import androidx.work.ListenableWorker;
 
+/**
+ * Not getting used anywhere currently, but can be used later on.
+ */
 public interface AuthenticatedRemoteUploader extends RemoteUploader{
     boolean isUploadingPermissionGranted(Context context);
 
